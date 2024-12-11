793dcda4f80a9cc68e970e3476224f71f7fe980b
Dhruv Dobhal <dhruvdobhal97@gmail.com>
2024-12-10 21:27:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,10 @@
-from flask import Flask, jsonify, request
-import socket
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def main():
-    return "Hello from Dobhal ECS Container"
+@app.route("/")
+def hello():
+    return "Hello from Dhruv ECS Container."
 
-@app.route('/host', methods=['GET'])
-def host():
-    hostname = socket.gethostname()
-    return f"Hostname: {hostname}"
-
-@app.route('/ip', methods=['GET'])
-def ip():
-    ip_address = request.remote_addr
-    return f"Client IP: {ip_address}"
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
